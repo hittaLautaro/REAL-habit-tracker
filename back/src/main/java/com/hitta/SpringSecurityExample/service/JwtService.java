4e5ba3d79afb152503cd3532ca2068eaedcee52d
hittaLautaro <hitta.lautaro@gmail.com>
2024-11-22 18:14:29 -0300

    Add env files

--- back/src/main/java/com/hitta/SpringSecurityExample/service/JwtService.java
+++ back/src/main/java/com/hitta/SpringSecurityExample/service/JwtService.java
@@ -1,16 +1,15 @@
 package com.hitta.SpringSecurityExample.service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-import java.security.NoSuchAlgorithmException;
-import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -19,18 +18,10 @@ import java.util.function.Function;
 @Service
 public class JwtService {
 
-
     private String secretkey;
 
-    public JwtService() {
-
-        try {
-            KeyGenerator keyGen = KeyGenerator.getInstance("HmacSHA256");
-            SecretKey sk = keyGen.generateKey();
-            secretkey = Base64.getEncoder().encodeToString(sk.getEncoded());
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
+    public JwtService(Dotenv dotenv) {
+        this.secretkey = dotenv.get("JWT_SECRET_KEY");
     }
 
     public String generateToken(String username) {
