2ca08eea96c05cc698c2935590afcb623dd2f8b7
hittaLautaro <hitta.lautaro@gmail.com>
2025-05-11 02:57:04 -0300

    Fixed auth routes vertical margin

--- front/src/pages/Login/page.jsx
+++ front/src/pages/Login/page.jsx
@@ -37,7 +37,7 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center my-5">
+    <div className="d-flex flex-column align-items-center justify-content-center">
       <div className="text-center mb-4 my-5">
         <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-yellow-400 fs-big sans-600">
           real.
--- front/src/pages/Signup/page.jsx
+++ front/src/pages/Signup/page.jsx
@@ -46,7 +46,7 @@ const RegisterPage = () => {
   };
 
   return (
-    <div className="d-flex flex-column align-items-center justify-content-center my-5">
+    <div className="d-flex flex-column align-items-center justify-content-center">
       <div className="text-center mb-4 my-5">
         <h1 className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-yellow-400 fs-big sans-600">
           real.
