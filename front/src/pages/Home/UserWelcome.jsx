68b2c7344279f511461ce4da356a62adcd76bc0e
hittaLautaro <hitta.lautaro@gmail.com>
2025-05-15 00:30:08 -0300

    changed userWelcome to user new variable name from users better loading return

--- front/src/pages/Home/UserWelcome.jsx
+++ front/src/pages/Home/UserWelcome.jsx
@@ -2,25 +2,6 @@ import React from "react";
 import "../../components/global/styles.css";
 
 const UserWelcome = ({ user }) => {
-  if (!user) {
-    return (
-      <div className="flex flex-col justify-center items-center my-12">
-        <div>
-          <h1 className="mono-600 text-center text-6xl">
-            <span className="text-white"> Hello </span>
-            <span className="relative inline-block">
-              <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-yellow-400">
-                Loading...
-              </span>
-              <span className="absolute left-0 -bottom-1 h-0.5 w-full bg-gradient-to-r from-green-500 to-yellow-400"></span>
-            </span>
-            <span className="text-white"> Hello </span>
-          </h1>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="flex flex-col justify-center items-center my-12">
       <div>
@@ -28,7 +9,7 @@ const UserWelcome = ({ user }) => {
           <span className="text-white"> Hello </span>
           <span className="relative inline-block">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-yellow-400">
-              {user.email.split("@")[0]}
+              {user ? user.name : "Unknown"}
             </span>
             <span className="absolute left-0 -bottom-1 h-0.5 w-full bg-gradient-to-r from-green-500 to-yellow-400"></span>
           </span>
